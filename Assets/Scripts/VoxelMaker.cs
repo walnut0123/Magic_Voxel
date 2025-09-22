01f8494f97bf09030438cbaeddd74ca638326e27
leegaeun <yse060524@gmail.com>
2025-09-22 11:17:45 +0900

    week 4

--- Assets/Scripts/VoxelMaker.cs
+++ Assets/Scripts/VoxelMaker.cs
@@ -12,6 +12,9 @@ public class VoxelMaker : MonoBehaviour
     public float createTime = 0.1f;
     float currentTime = 0;
 
+    //크로스헤어 변수
+    public Transform crosshair;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,34 +29,41 @@ public class VoxelMaker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        currentTime += Time.deltaTime;
-        if(currentTime > createTime)
+        //크로스헤어 그리기
+        ARAVRInput.DrawCrosshair(crosshair);
+        if (ARAVRInput.Get(ARAVRInput.Button.One))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hitInfo = new RaycastHit();
-            if(Physics.Raycast(ray,out hitInfo)) //마우스가 바닥에 위치해있다면,
+            currentTime += Time.deltaTime;
+            if(currentTime > createTime)
             {
-                if(voxelPool.Count > 0)
+                //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                Ray ray = new Ray(ARAVRInput.RHandPosition,ARAVRInput.RHandDirection);
+                RaycastHit hitInfo = new RaycastHit();
+                if(Physics.Raycast(ray,out hitInfo)) //마우스가 바닥에 위치해있다면,
                 {
-                    GameObject voxel = voxelPool[0];
-                    voxel.SetActive(true);
-                    voxel.GetComponent<Renderer>().material.color = new Color(Random.value,Random.value,Random.value);
-                    voxel.transform.position = hitInfo.point;
-                    voxelPool.RemoveAt(0);
-                    currentTime = 0;
+                    if(voxelPool.Count > 0)
+                    {
+                        GameObject voxel = voxelPool[0];
+                        voxel.SetActive(true);
+                        voxel.GetComponent<Renderer>().material.color = new Color(Random.value,Random.value,Random.value);
+                        voxel.transform.position = hitInfo.point;
+                        voxelPool.RemoveAt(0);
+                        currentTime = 0;
+
+                    }
+                    else
+                    {
+                        GameObject voxel = Instantiate(voxelFactory);
+                        voxel.transform.position = hitInfo.point;
+                    }
 
                 }
-                else
-                {
-                    GameObject voxel = Instantiate(voxelFactory);
-                    voxel.transform.position = hitInfo.point;
-                }
-
+            }
+            if (Input.GetButtonDown("Fire1"))
+            {
+            
             }
         }
-        if (Input.GetButtonDown("Fire1"))
-        {
-            
-        }
+
     }
 }
